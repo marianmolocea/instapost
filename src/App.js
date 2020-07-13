a915fe3c19c9b3e3ae174a19e24381aea4248e81
marianmolocea <marian.mcrt@gmail.com>
2020-07-13 11:50:10 +0100

    Create the top navigation bat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './frontend/components/NavBar/NavBar'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      Hello to InstaPost
+      <NavBar />
     </div>
   );
 }
---
+++ src/frontend/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <div className="NavBar bottom-box-shadow">
+            Instapost
+        </div>
+    )
+}
+
+export default NavBar
