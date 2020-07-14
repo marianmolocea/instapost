3fd82e595978d6e37bac9552b2f62aabb5f750c0
marianmolocea <marian.mcrt@gmail.com>
2020-07-14 11:20:40 +0100

    Add navigation icons

--- src/frontend/components/NavBar/NavBar.js
+++ src/frontend/components/NavBar/NavBar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { AiOutlineHome, AiOutlineHeart, AiOutlineCompass } from 'react-icons/ai' 
+import { FiSend, FiUser } from 'react-icons/fi' 
+
 import './NavBar.scss'
 
 const NavBar = () => {
     return (
         <div className="NavBar bottom-box-shadow">
-            Instapost
+            <div className="logo">Instapost</div>
+            <div className="icons-container">
+                <AiOutlineHome />
+                <AiOutlineCompass />
+                <AiOutlineHeart />
+                <FiSend />
+                <FiUser />
+            </div>
         </div>
     )
 }
