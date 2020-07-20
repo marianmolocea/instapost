3ba61328162b0ef486557c1575ee9ea3f500b7a3
marianmolocea <marian.mcrt@gmail.com>
2020-07-20 09:44:43 +0100

    style the coment input

--- src/frontend/components/PostView/PostView.js
+++ src/frontend/components/PostView/PostView.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './PostView.css'
 import Avatar from './Avatar/Avatar'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { RiChat3Line } from 'react-icons/ri'
+import { FiSend } from 'react-icons/fi'
 
 const PostView = ({userName, image, likeNumber, caption}) => {
+
+    const iconSize = "25px";
+
     return (
         <div className="PostView bottom-box-shadow">
             <div className="post-header">
@@ -10,11 +16,18 @@ const PostView = ({userName, image, likeNumber, caption}) => {
                 <div className="element">{userName}</div> 
             </div>
             <img src={image} alt="text" className="image" />
-            <div className="actions"></div>
+            <div className="actions">
+                <AiOutlineHeart size={iconSize} />
+                <RiChat3Line size={iconSize}/>
+                <FiSend size={iconSize}/>
+            </div>
             <div className="likes-number">There are <b>{likeNumber} people</b> who like your post</div>
             <div className="caption">{caption}</div>
             <div className="comments"></div>
-            <div className="add-comment"></div>
+            <div className="add-comment-container">
+                <input className="add-comment" placeholder="Add a comment..." />
+                <button className="post">Post</button>
+            </div>
         </div>
     )
 }
