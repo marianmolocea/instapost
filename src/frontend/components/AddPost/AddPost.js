2629bb8a53e19cabe111e95f2de3e68a9343c0da
marianmolocea <marian.mcrt@gmail.com>
2020-07-21 13:40:23 +0100

    add drag and drop component

--- src/frontend/components/AddPost/AddPost.js
+++ src/frontend/components/AddPost/AddPost.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './AddPost.css'
+import Dropzone from 'react-dropzone'
+
+
 
 const AddPost = () => {
     return (
-        <div className="AddPost">
-            
+        <div className="AddPost bottom-box-shadow">
+            <div className="drag-drop">
+                <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                    {({getRootProps, getInputProps}) => (
+                        <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                        </div>
+                        </section>
+                    )}
+                </Dropzone>
+            </div>
+            <textarea className="caption" placeholder="Caption Here" />
+            <button className="add-post">Post</button>
         </div>
     )
 }
